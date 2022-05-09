eb3cf68871da00ddd57291cf5b0b949c147713dc
knamuuu <knamuuu@naver.com>
2022-05-10 01:45:06 +0900

    Modify Route: Layout children={<component />}

--- src/App.js
+++ src/App.js
@@ -4,33 +4,36 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/view/Layout/Layout";
 import GoodsTest from "./components/view/Goods/GoodsTest";
 import GoodsAdd from "./components/view/Goods/GoodsAdd";
-import GoodsList from "./components/view/Goods/GoodsList";
+import GoodsInfo from "./components/view/Goods/GoodsInfo";
 import LoginPage from "./components/view/LoginPage/LoginPage";
 import RegisterPage from "./components/view/RegisterPage/RegisterPage";
 
 import "./App.css";
 import LandingPage from "./components/view/LandingPage/LandingPage";
 
-const REST_API_KEY = "f9f1df64f45106996eef35693c66965e";
-const REDIRECT_URI = "http://localhost:3000/oauth/kakao/callback";
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route exact path="/" element={<LandingPage />} />
+          <Route
+            exact
+            path="/"
+            element={<Layout children={<LandingPage />} />}
+          />
 
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<Layout children={<LoginPage />} />} />
+          <Route
+            path="/register"
+            element={<Layout children={<RegisterPage />} />}
+          />
 
           <Route path="/goods" element={<Layout children={<GoodsTest />} />} />
           <Route
             path="/goods/add"
             element={<Layout children={<GoodsAdd />} />}
           />
-          <Route path="/goods/item" element={<GoodsList />} />
+          <Route path="/goods/item" element={<GoodsInfo />} />
         </Routes>
       </div>
     </Router>
