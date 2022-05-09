af8b3644b8d7519d8c52d1ab82e331ba0567d94f
knamuuu <knamuuu@naver.com>
2022-05-09 15:33:37 +0900

    global font 적용

---
+++ src/font/font.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    @font-face {
+        font-family: "S-CoreDream-9Black";
+        src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-9Black.woff")
+          format("woff");
+        font-style: normal;
+  }
+`;
