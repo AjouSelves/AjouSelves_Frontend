9e8a7d5e6f0f0d88893d5c5b812bace2ff82ccb6
knamuuu <knamuuu@naver.com>
2022-05-10 14:54:11 +0900

    불필요한 코드 삭제

--- src/components/view/Layout/Layout.js
+++ src/components/view/Layout/Layout.js
@@ -3,26 +3,11 @@ import styled from "styled-components";
 
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import main_image from "../../../images/main_image.png";
-import GlobalFonts from "../../../font/font";
 
 const StyledDiv = styled.div`
   padding-top: 65px;
   display: flex;
   flex-direction: column;
-`;
-
-const MainP = styled.span`
-  font-family: "S-CoreDream-9Black";
-
-  position: absolute;
-  top: 55%;
-  left: 55%;
-
-  color: white;
-  font-weight: 900;
-  font-size: 54px;
-  text-align: right;
 `;
 
 function Layout({ children }) {
